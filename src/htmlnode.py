0b87a6cbfba7e1c13f0bc00ef312bea1a2802433
Saturnfive <bevane@live.com>
2024-06-07 14:50:31 +0700

    fix: fix value error handling for leafnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -33,9 +33,9 @@ class LeafNode(HTMLNode):
 
 
     def to_html(self):
-        if not self.value:
+        if self.value is None:
             raise ValueError("LeafNode must have a value")
-        if not self.tag:
+        if self.tag is None:
             return self.value
         props_html = self.props_to_html()
         return f"<{self.tag}{props_html}>{self.value}</{self.tag}>"
