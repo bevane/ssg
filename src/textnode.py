57c26c934262372ecdc3e3c518b8f77d3654bd73
Saturnfive <bevane@live.com>
2024-06-11 13:07:34 +0700

    refactor: move text_node_to_html

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,6 @@
+from htmlnode import LeafNode
+
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -18,3 +21,24 @@ class TextNode:
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
 
+
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    text_types_to_html_args = {
+        "text": {"value": text_node.text},
+        "bold": {"tag": "b", "value": text_node.text},
+        "italic": {"tag": "i", "value": text_node.text},
+        "code": {"tag": "code", "value": text_node.text},
+        "link": {
+            "tag": "a",
+            "props": {"href": text_node.url}, "value": text_node.text
+        },
+        "image": {"tag": "img",
+                  "props": {"src": text_node.url, "alt": text_node.text},
+                  "value": ""
+        },
+    }
+
+    if not text_node.text_type in text_types_to_html_args:
+        raise ValueError(f"{text_node.text_type} is not a valid text type")
+    html_node = LeafNode(**text_types_to_html_args[text_node.text_type])
+    return html_node
