b7988f369fcc766272496e0507779ac28a5ebe80
Saturnfive <bevane@live.com>
2024-06-11 13:08:13 +0700

    test: add test for text_node_to_html_node

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode
+from textnode import TextNode, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -22,5 +22,29 @@ class TestTextNode(unittest.TestCase):
         self.assertNotEqual(node, node2)
 
 
+class TestTextNodetoHTMLNode(unittest.TestCase):
+    def test_bold(self):
+        text_node = TextNode(text="bold text", text_type="bold")
+        html = "<b>bold text</b>"
+        self.assertEqual(text_node_to_html_node(text_node).to_html(), html)
+
+
+    def test_url(self):
+        text_node = TextNode(
+            text="click me", text_type="link", url="www.boot.dev"
+        )
+        html = '<a href="www.boot.dev">click me</a>'
+        self.assertEqual(text_node_to_html_node(text_node).to_html(), html)
+
+
+    def test_image(self):
+        text_node = TextNode(
+            text="small cat", text_type="image", url="cat.png"
+        )
+        html = '<img src="cat.png" alt="small cat"></img>'
+        self.assertEqual(text_node_to_html_node(text_node).to_html(), html)
+
+
 if __name__ == "__main__":
     unittest.main()
+
