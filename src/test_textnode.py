3f7928dc7673135e51325a255d162b915d657af1
Saturnfive <bevane@live.com>
2024-06-11 13:48:47 +0700

    test: add more tests for split_node

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-
+from htmlnode import LeafNode
 from textnode import TextNode, text_node_to_html_node, split_nodes_delimiter
 
 
@@ -48,14 +48,81 @@ class TestTextNodetoHTMLNode(unittest.TestCase):
 class SplitNodes(unittest.TestCase):
     def test_bold(self):
         old_nodes = [
-                TextNode(
+            TextNode(
                 text="This is text with a **bolded** word", text_type="text"
             ),
         ]
         new_nodes = [
             TextNode("This is text with a ", "text"),
             TextNode("bolded", "bold"),
-            TextNode(" word", "text"),
+            TextNode(" word", "text")
+        ]
+        self.assertEqual(
+            split_nodes_delimiter(old_nodes, "**", "bold"), new_nodes
+        )
+
+
+    def test_italic(self):
+        old_nodes = [
+            TextNode(
+                text="This is text with an *italic* word", text_type="text"
+            ),
+        ]
+        new_nodes = [
+            TextNode("This is text with an ", "text"),
+            TextNode("italic", "italic"),
+            TextNode(" word", "text")
+        ]
+        self.assertEqual(
+            split_nodes_delimiter(old_nodes, "*", "italic"), new_nodes
+        )
+
+
+    def test_code(self):
+        old_nodes = [
+            TextNode(
+                text="This is text with a `code block` word", text_type="text"
+            ),
+        ]
+        new_nodes = [
+            TextNode("This is text with a ", "text"),
+            TextNode("code block", "code"),
+            TextNode(" word", "text")
+        ]
+        self.assertEqual(
+            split_nodes_delimiter(old_nodes, "`", "code"), new_nodes
+        )
+
+
+    def test_bold_different_words(self):
+        old_nodes = [
+            TextNode(
+                text="This is text with a **bolded** word and **another**",
+                text_type="text"
+            ),
+        ]
+        new_nodes = [
+            TextNode("This is text with a ", "text"),
+            TextNode("bolded", "bold"),
+            TextNode(" word and ", "text"),
+            TextNode("another", "bold")
+        ]
+        self.assertEqual(
+            split_nodes_delimiter(old_nodes, "**", "bold"), new_nodes
+        )
+
+
+    def test_bold_multi_words(self):
+        old_nodes = [
+            TextNode(
+                text="This is text with **bolded words**.",
+                text_type="text"
+            ),
+        ]
+        new_nodes = [
+            TextNode("This is text with ", "text"),
+            TextNode("bolded words", "bold"),
+            TextNode(".", "text"),
         ]
         self.assertEqual(
             split_nodes_delimiter(old_nodes, "**", "bold"), new_nodes
